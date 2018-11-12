c4dd2e79c75ed8978fa9d3f6e5a25515da7376eb
nyuscovich <43142430+nyuscovich@users.noreply.github.com>
2018-11-12 17:20:31 +0200

    Create ver2.py

---
+++ SKRIPTI IDLE- -sin_caur_summu_ver1/ver2.py
@@ -0,0 +1,67 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: /home/user/sin_caur_summu_ver1.py =================
+Lietotāj, lūdzu, ievadi argumentu (x): 0.25
+sin(0.25) = 0.25
+a0 = 0.25 S0 = 0.25
+a1 = -0.00 S1 = 0.25
+a2 = 0.00 S2 = 0.25
+a3 = -0.00 S3 = 0.25
+>>> 
+================= RESTART: /home/user/sin_caur_summu_ver1.py =================
+Lietotāj, lūdzu, ievadi argumentu (x): 1250
+sin(1250.00) = -0.35
+a0 = 1250.00 S0 = 1250.00
+a1 = -325520833.33 S1 = -325519583.33
+a2 = 25431315104166.67 S2 = 25430989584583.34
+a3 = -946105472625248000.00 S3 = -946080041635663360.00
+>>> 
+================= RESTART: /home/user/sin_caur_summu_ver1.py =================
+Lietotāj, lūdzu, ievadi argumentu (x): 0.98
+sin(0.98) = 0.83
+a0 = 0.98 S0 = 0.98
+a1 = -0.16 S1 = 0.82
+a2 = 0.01 S2 = 0.83
+a3 = -0.00 S3 = 0.83
+>>> 
+================= RESTART: /home/user/sin_caur_summu_ver1.py =================
+Lietotāj, lūdzu, ievadi argumentu (x): a
+Traceback (most recent call last):
+  File "/home/user/sin_caur_summu_ver1.py", line 11, in <module>
+    x = float(input("Lietotāj, lūdzu, ievadi argumentu (x): "))
+ValueError: could not convert string to float: 'a'
+>>> 
+================= RESTART: /home/user/sin_caur_summu_ver1.py =================
+Lietotāj, lūdzu, ievadi argumentu (x): 25
+sin(25.00) = -0.13
+a0 = 25.00 S0 = 25.00
+a1 = -2604.17 S1 = -2579.17
+a2 = 81380.21 S2 = 78801.04
+a3 = -1211015.00 S3 = -1132213.96
+>>> sin(0.25)
+0.24740395925452294
+>>> sin(0)
+0.0
+>>> sin(1)
+0.8414709848078965
+>>> sin(0.5)
+0.479425538604203
+>>> 
+================= RESTART: /home/user/sin_caur_summu_ver2.py =================
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) = 0.84
+a0 = 1.00 S0 = 1.00
+a1 = -0.17 S1 = 0.83
+a2 = 0.01 S2 = 0.84
+a3 = -0.00 S3 = 0.84
+>>> 
+================= RESTART: /home/user/sin_caur_summu_ver2.py =================
+Lietotāj, lūdzu, ievadi argumentu (x): 0.25
+sin(0.25) = 0.25
+a0 = 0.25 S0 = 0.25
+a1 = -0.00 S1 = 0.25
+a2 = 0.00 S2 = 0.25
+a3 = -0.00 S3 = 0.25
+>>> 
