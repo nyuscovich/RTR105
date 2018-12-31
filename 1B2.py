87ac3766c327c0a381e81911dde3cbd56b8e2b59
nyuscovich <43142430+nyuscovich@users.noreply.github.com>
2018-12-31 13:02:17 +0200

    Update 1B2.py

--- 1B2.py
+++ 1B2.py
@@ -1,23 +1,27 @@
 import sys
 sys.path.append('/usr/local/anaconda3/lib/python3.6/site-packages')
 
-from numpy import *
-x = linspace(0, 4, 70)
-y = sin(x)
-b = x
-c = x - x**3/(3*2*1)
-d = x - x**3/(3*2*1) + x**5/(5*4*3*2*1)
-e = x - x**3/(3*2*1) + x**5/(5*4*3*2*1) - x**7/(7*6*5*4*3*2*1)
+from numpy import sin, linspace
+x = linspace(0, 4, 11)
+y= sin(x)
+y1= x
+#y2= x-x*x*x/(1*2*3)
+y2= y1 - x**3/(1*2*3)
+#y3= x-x*x*x/(1*2*3) + x*x*x*x*x/(1*2*3*4*5)
+y3= y2 - x**5/(1*2*3*4*5)
+#y4= x-x*x*x/(1*2*3) + x*x*x*x*x/(1*2*3*4*5) - x*x*x*x*x*x*x/(1*2*3*4*5*6*7)
+y4= y3 - x**7/(1*2*3*4*5*6*7)
 
 from matplotlib import pyplot as plt
 plt.grid()
 plt.xlabel('x')
 plt.ylabel('f(x)')
-plt.title('Funkcija $sin(x)$')
-plt.plot(x, y)
-plt.plot(x, y, color = "#FFA6C9")
-plt.plot(x, b, color = "#000000")
-plt.plot(x, c, color = "#0000FF")
-plt.plot(x, d, color = "#FF0000")
-plt.plot(x, e, color = "#00FF00")
+plt.title('Funkcija$ cos(x) un sin(x)$')
+plt.plot(x, y, color = '#FF0098')
+plt.plot(x, y1, color = '#00FF11')
+plt.plot(x, y2, color = '#0000FF')
+plt.plot(x, y3, color = '#FFFF00')
+plt.plot(x, y4, color = '#00FFFF')
+
+plt.legend(['$cos(x)$', '$x$', '$x-x^3/3!$', '$x-x^3/3!+x^5/5!$', '$x-x^3/3!+x^5/5!-x^7/7!$'])
 plt.show()
